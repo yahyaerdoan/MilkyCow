3a4bc8486e556e2c4d4b65c26fae63012aa2c418
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-23 07:02:22 -0500

    Entity added.

---
+++ MilkyCow.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkyCow.EntityLayer.Concrete
+{
+	public class Product
+	{
+        public int PoductId { get; set; }
+        public string Name { get; set; }
+        public decimal LastPrice { get; set; }
+        public decimal NewPrice { get; set; }
+    
+    }
+
+	public class Slider
+	{
+		public int SliderId { get; set; }
+
+		public string Name { get; set; }
+		public decimal LastPrice { get; set; }
+		public decimal NewPrice { get; set; }
+
+	}
+}
