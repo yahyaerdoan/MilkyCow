70c5d559e282371baf488559ac2842a096112a0c
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-24 22:59:10 -0500

    Entities created.

--- MilkyCow.EntityLayer/Concrete/Product.cs
+++ MilkyCow.EntityLayer/Concrete/Product.cs
@@ -6,22 +6,14 @@ using System.Threading.Tasks;
 
 namespace MilkyCow.EntityLayer.Concrete
 {
-	public class Product
-	{
-        public int PoductId { get; set; }
-        public string Name { get; set; }
-        public decimal LastPrice { get; set; }
-        public decimal NewPrice { get; set; }
-    
-    }
 
-	public class Slider
+    public class Product
 	{
-		public int SliderId { get; set; }
-
-		public string Name { get; set; }
+		public int ProductId { get; set; }
+		public string? Name { get; set; }
 		public decimal LastPrice { get; set; }
 		public decimal NewPrice { get; set; }
-
+        public string? Image { get; set; }
+        public bool Status { get; set; }
 	}
 }
---
+++ MilkyCow.EntityLayer/Concrete/Slider.cs
@@ -0,0 +1,12 @@
+﻿namespace MilkyCow.EntityLayer.Concrete
+{
+    public class Slider
+    {
+        public int SliderId { get; set; }
+        public string? Image { get; set; }
+        public string? Header { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool Status { get; set; }
+    }  
+}
