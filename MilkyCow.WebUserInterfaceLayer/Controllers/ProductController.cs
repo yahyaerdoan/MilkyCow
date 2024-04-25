8ae01074b2249f20080c1f28f48283c3413ac3f4
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-25 07:43:30 -0500

    Product listed.

---
+++ MilkyCow.WebUserInterfaceLayer/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilkyCow.WebUserInterfaceLayer.Dtos;
+using Newtonsoft.Json;
+
+namespace MilkyCow.WebUserInterfaceLayer.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44367/api/Product");
+            if (responseMessage.IsSuccessStatusCode) 
+            { 
+                var jonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
