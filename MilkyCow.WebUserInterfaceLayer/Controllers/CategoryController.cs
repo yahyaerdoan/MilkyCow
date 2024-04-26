72f21c895f03c2fa349f71377bc0e2c28e05f39d
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-04-26 07:02:00 -0500

    Worked on category

--- MilkyCow.WebUserInterfaceLayer/Controllers/CategoryController.cs
+++ MilkyCow.WebUserInterfaceLayer/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MilkyCow.WebUserInterfaceLayer.Dtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MilkyCow.WebUserInterfaceLayer.Controllers
 {
@@ -23,6 +24,65 @@ namespace MilkyCow.WebUserInterfaceLayer.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
                 return View(values);
 
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44367/api/Category/Category/Create", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+       
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();       
+            var responseMessage = await client.DeleteAsync("https://localhost:44367/api/Category/Category/Delete?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();        
+            var responseMessage = await client.GetAsync("https://localhost:44367/api/Category/Category/GetById?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44367/api/Category/Category/Update", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
---
+++ MilkyCow.WebUserInterfaceLayer/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MilkyCow.WebUserInterfaceLayer.Dtos
+{
+    public class CreateCategoryDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
