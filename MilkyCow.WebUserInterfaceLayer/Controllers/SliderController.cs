a630eb99124e221217535db21cab879ebef3aa72
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-05-14 22:57:31 -0500

    Sliders listed.

---
+++ MilkyCow.WebUserInterfaceLayer/Controllers/SliderController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilkyCow.WebUserInterfaceLayer.Dtos;
+using Newtonsoft.Json;
+
+namespace MilkyCow.WebUserInterfaceLayer.Controllers
+{
+    public class SliderController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public SliderController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            #region option 1
+            //var baseUrl = _configuration["AppSettings:BaseApiUrl"];
+            //var client = _httpClientFactory.CreateClient();
+            //var response = await client.GetAsync($"{baseUrl}Sliders/GetAllSliders");
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    var jsonData = await response.Content.ReadAsStringAsync();
+            //    var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+            //    return View(values);
+            //}
+            //return View();
+            #endregion
+
+            #region option 2
+            //var baseUrl = _configuration["AppSettings:BaseApiUrl"];
+            //var client = _httpClientFactory.CreateClient();
+
+            //var values = await client.GetFromJsonAsync<List<ResultSliderDto>>($"{baseUrl}Sliders/GetAllSliders");
+            //return View(values ?? new List<ResultSliderDto>()); // Return an empty list if values are null
+            #endregion
+
+            #region option 3
+            var baseUrl = _configuration["AppSettings:BaseApiUrl"];
+
+            var values = await _httpClientFactory.CreateClient()
+                .GetFromJsonAsync<List<ResultSliderDto>>($"{baseUrl}Sliders/GetAllSliders");
+
+            return View(values ?? new List<ResultSliderDto>());
+            #endregion
+        }
+    }
+}
---
+++ MilkyCow.WebUserInterfaceLayer/Dtos/ResultSliderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MilkyCow.WebUserInterfaceLayer.Dtos
+{
+    public class ResultSliderDto
+    {
+        public int SliderId { get; set; }
+        public string? Image { get; set; }
+        public string? Header { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
