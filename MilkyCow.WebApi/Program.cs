4482b9c97439c4af6c525a7461f2d4777481d12d
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-05-15 12:06:21 -0500

    Parrameter name changed.

--- MilkyCow.WebApi/Program.cs
+++ MilkyCow.WebApi/Program.cs
@@ -45,18 +45,18 @@ app.UseAuthorization();
 
 app.MapControllers(); 
 
-app.MapGet("/MinimalApiProducts", (IProductService _dbontext) =>
+app.MapGet("/MinimalApiProducts", (IProductService _productService) =>
 {
-	return  _dbontext.GetAll().ToList();
+	return _productService.GetAll().ToList();
 });
 
-app.MapGet("/MinimalApiProducts/{id}", (string id, IProductService _dbontext) => {
+app.MapGet("/MinimalApiProducts/{id}", (string id, IProductService _productService) => {
 
 	if (!int.TryParse(id, out int productId) || productId <= 0)
 	{
 		return Results.BadRequest("Invalid ID format. ID must be a numeric value and a positive integer.");
     }
-	var product = _dbontext.GetById(productId);
+	var product = _productService.GetById(productId);
 	if (product != null)
 	{
 		return Results.Ok(product);
@@ -67,16 +67,16 @@ app.MapGet("/MinimalApiProducts/{id}", (string id, IProductService _dbontext) =>
     }
 });
 
-app.MapPost("/MinimalApiCreateProduct", (IProductService _post, Product product) =>
+app.MapPost("/MinimalApiCreateProduct", (IProductService _productService, Product product) =>
 {
-    _post.Add(product);
+    _productService.Add(product);
 
     return Results.Ok(product);
 });
 
-app.MapPut("/MinimalApiUpdateProduct/{id}", (int id, IProductService _post, Product updatedProduct) =>
+app.MapPut("/MinimalApiUpdateProduct/{id}", (int id, IProductService _productService, Product updatedProduct) =>
 {
-    var existingProduct = _post.GetById(id);
+    var existingProduct = _productService.GetById(id);
     if (existingProduct == null)
     {
         return Results.BadRequest("Invalid product ID. Product does not exist.");
@@ -85,34 +85,34 @@ app.MapPut("/MinimalApiUpdateProduct/{id}", (int id, IProductService _post, Prod
     {
         return Results.BadRequest("Invalid category ID. Category does not match the product's category.");
     }
-    _post.Update(existingProduct);
+    _productService.Update(existingProduct);
     return Results.Ok(existingProduct);
 });
 
-app.MapDelete("/MinimalApiDeleteProduct/{id}", (int id, IProductService _post) =>
+app.MapDelete("/MinimalApiDeleteProduct/{id}", (int id, IProductService _productService) =>
 {
-    var existingProduct = _post.GetById(id);
+    var existingProduct = _productService.GetById(id);
     if (existingProduct == null)
     {
         return Results.BadRequest("Invalid product ID. Product does not exist.");
     }
-    _post.Delete(existingProduct.ProductId);
+    _productService.Delete(existingProduct.ProductId);
     return Results.Ok(existingProduct);
 });
 
-app.MapPost("/MinimalApiMultipleDeleteProducts/{ids}", (string ids, IProductService _post) =>
+app.MapPost("/MinimalApiMultipleDeleteProducts/{ids}", (string ids, IProductService _productService) =>
 {
     // Split the comma-separated string of IDs into an array of integers
     var idArray = ids.Split(',').Select(int.Parse).ToArray();
 
     foreach (var id in idArray)
     {
-        var existingProduct = _post.GetById(id);
+        var existingProduct = _productService.GetById(id);
         if (existingProduct == null)
         {
             return Results.BadRequest($"Invalid product ID {id}. Product does not exist.");
         }
-        _post.Delete(existingProduct.ProductId);
+        _productService.Delete(existingProduct.ProductId);
     }
     return Results.Ok($"Deleted product ID {ids}");
 });
