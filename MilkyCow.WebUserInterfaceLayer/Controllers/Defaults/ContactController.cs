0a26fbafac2541c4fa9c0e8b2207a1b6027ce295
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-05-16 18:45:08 -0500

    ControllerCreated.

---
+++ MilkyCow.WebUserInterfaceLayer/Controllers/Defaults/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkyCow.WebUserInterfaceLayer.Controllers.Defaults
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MilkyCow.WebUserInterfaceLayer/Controllers/Defaults/OurTeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkyCow.WebUserInterfaceLayer.Controllers.Defaults
+{
+    public class OurTeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
