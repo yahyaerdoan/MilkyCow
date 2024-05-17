8bcb3e8d51ea155182ae6c6f2aeb677441d7cd6e
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-05-16 20:37:37 -0500

    Dynamic title developed. Code refactored.

---
+++ MilkyCow.WebUserInterfaceLayer/ViewComponents/PageHeaderViewComponents/PageHeaderViewComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkyCow.WebUserInterfaceLayer.ViewComponents.PageHeaderViewComponents
+{
+    public class PageHeaderViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke(string title) { return View("Default", title); }
+    }
+}
