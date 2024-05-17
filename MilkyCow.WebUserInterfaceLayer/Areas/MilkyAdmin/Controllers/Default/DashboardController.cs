3458bd774128956675e4f3fc53968a7148d74870
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-05-17 09:43:23 -0500

    Layout created.

---
+++ MilkyCow.WebUserInterfaceLayer/Areas/MilkyAdmin/Controllers/Default/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkyCow.WebUserInterfaceLayer.Areas.MilkyAdmin.Controllers.Default
+{
+    [Area("MilkyAdmin")]
+    [Route("MilkyAdmin/{controller=Home}/{action=Index}/{id?}")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MilkyCow.WebUserInterfaceLayer/Areas/MilkyAdmin/Controllers/Layout/LayoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkyCow.WebUserInterfaceLayer.Areas.MilkyAdmin.Controllers.Layout
+{
+    [Area("MilkyAdmin")]
+    [Route("MilkyAdmin/{controller=Home}/{action=Index}/{id?}")]
+    public class LayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
