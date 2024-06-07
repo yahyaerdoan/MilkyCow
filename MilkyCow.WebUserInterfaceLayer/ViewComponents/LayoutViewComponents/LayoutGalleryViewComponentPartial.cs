c9c2cfe5a26e9a22078f396d0b4341a997581139
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-06-06 23:24:26 -0500

    Galleries listed.

--- MilkyCow.WebUserInterfaceLayer/ViewComponents/LayoutViewComponents/LayoutGalleryViewComponentPartial.cs
+++ MilkyCow.WebUserInterfaceLayer/ViewComponents/LayoutViewComponents/LayoutGalleryViewComponentPartial.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MilkyCow.DataTransferObjectLayer.Concrete.GalleryDtos;
+using MilkyCow.WebUserInterfaceLayer.Extensions.DynamicBaseConsume;
 
 namespace MilkyCow.WebUserInterfaceLayer.ViewComponents.LayoutViewComponents
 {
     public class LayoutGalleryViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() {  return View(); }
+        private readonly DynamicConsume<ResultGalleryDto> _resultGalleryDto;
+
+        public LayoutGalleryViewComponentPartial(DynamicConsume<ResultGalleryDto> resultGalleryDto)
+        {
+            _resultGalleryDto = resultGalleryDto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            var values = await _resultGalleryDto.GetListAsync("Galleries/GalleryList");
+            return View(values); 
+        }
     }
 }
